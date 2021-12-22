d21882c72795a9ad5446833f2bb31fb1d3563fae
Felipe Faciroli <felipef.dev@gmail.com>
2021-12-22 16:40:28 -0300

    adjustment export themes and amojis

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
 
 import Layout from './components/Layout';
-import Dashboard from './pages/Dashboard';
 import List from './pages/List';
 import dark from './styles/themes/dark';
 
