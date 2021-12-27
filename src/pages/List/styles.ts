abce9a07191ab8658589aeb4f6abb3d807637857
Felipe Faciroli <felipef.dev@gmail.com>
2021-12-27 16:55:38 -0300

    feat/27: filtered by finance moviment

--- src/pages/List/styles.ts
+++ src/pages/List/styles.ts
@@ -21,6 +21,7 @@ export const Filters = styled.div`
     color: ${props => props.theme.colors.white};
     margin: 0 10px;
     transition: opacity .3s;
+    opacity: .4;
 
     &:hover{
       opacity: .7;
@@ -32,7 +33,7 @@ export const Filters = styled.div`
     display: block;
     width: 55px;
     margin: 0 auto;
-    border-bottom: 10px solid ${props => props.theme.colors.warning};
+    border-bottom: 10px solid ${props => props.theme.colors.success};
   }
 
   .tag-filter-eventual::after{
@@ -40,6 +41,10 @@ export const Filters = styled.div`
     display: block;
     width: 55px;
     margin: 0 auto;
-    border-bottom: 10px solid ${props => props.theme.colors.success};
+    border-bottom: 10px solid ${props => props.theme.colors.warning};
+  }
+
+  .tag-actived{
+    opacity: 1;
   }
 `;
