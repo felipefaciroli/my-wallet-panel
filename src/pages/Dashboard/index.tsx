3edde9df4840b2f45718ff47bfa68238adc98645
Felipe Faciroli <felipef.dev@gmail.com>
2021-12-28 15:53:30 -0300

    feat/30:

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,23 +1,89 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 
 import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
+
+import expenses from '../../repositories/expenses';
+import gains from '../../repositories/gains';
+import listOfMonths from '../../utils/months';
 
 import {
   Container
 } from './styles';
 
 const Dashboard: React.FC = () => {
+  const [monthSelected, setMonthSelected] = useState<number>(new Date().getMonth() + 1);
+  const [yearSelected, setYearSelected] = useState<number>(new Date().getFullYear());
+
   const options = [
     { value: 'Felipe', label: 'Rodrigo' },
     { value: 'Ana', label: 'Ana' },
     { value: 'João', label: 'João' }
   ]
 
+  const years = useMemo(() => {
+    let uniqueYears: number[] = [];
+
+    [...expenses, ...gains].forEach(item => {
+      const date = new Date(item.date);
+      const year = date.getFullYear();
+
+      if (!uniqueYears.includes(year)) {
+        uniqueYears.push(year);
+      }
+    });
+
+    return uniqueYears.map(year => {
+      return {
+        value: year,
+        label: year
+      }
+    });
+  }, []);
+
+
+  const months = useMemo(() => {
+    return listOfMonths.map((month, index) => {
+      return {
+        value: index + 1,
+        label: month
+      }
+    });
+  }, []);
+
+  const handleMonthSelected = (month: string) => {
+    try {
+      const parseMonth = Number(month);
+      setMonthSelected(parseMonth);
+    } catch (error) {
+      throw new Error('invalid month value. Id accept 0 - 24.');
+    }
+  };
+
+
+  const handleYearSelected = (year: string) => {
+    try {
+      const parseYear = Number(year);
+      setYearSelected(parseYear);
+    } catch (error) {
+      throw new Error('invalid year value. Id accept integer numbers.');
+    }
+  };
+
   return (
     <Container>
       <ContentHeader title="Dashboard" lineColor='#f7931b'>
-        <SelectInput options={options} onChange={() => { }} />
+        <SelectInput
+          options={months}
+          defaultValue={monthSelected}
+          onChange={(e) => handleMonthSelected(e.target.value)}
+        />
+
+        <SelectInput
+          options={years}
+          defaultValue={yearSelected}
+          onChange={(e) => handleYearSelected(e.target.value)}
+        />
       </ContentHeader>
     </Container>
   )
