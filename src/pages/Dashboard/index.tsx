90a9ecbb3cd1c04b4bbe39429cf1b97a1ec097b7
Felipe Faciroli <felipef.dev@gmail.com>
2021-12-29 14:44:25 -0300

    feat/33: Leaving cards with dynamic information

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useMemo } from 'react';
 
 import happyImg from '../../assets/happy.svg';
 import sadImg from '../../assets/sad.svg';
+import grinningImg from '../../assets/grinning.svg';
 
 import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
@@ -51,11 +52,84 @@ const Dashboard: React.FC = () => {
     });
   }, []);
 
+
+  const totalExpenses = useMemo(() => {
+    let total: number = 0;
+
+    expenses.forEach(item => {
+      const date = new Date(item.date);
+      const year = date.getFullYear();
+      const month = date.getMonth() + 1;
+
+      if (month === monthSelected && year === yearSelected) {
+        try {
+          total += Number(item.amount);
+        } catch {
+          throw new Error('Invalid amount! Amount must be number.');
+        }
+      }
+    });
+
+    return total;
+  }, [monthSelected, yearSelected]);
+
+  const totalGains = useMemo(() => {
+    let total: number = 0;
+
+    gains.forEach(item => {
+      const date = new Date(item.date);
+      const year = date.getFullYear();
+      const month = date.getMonth() + 1;
+
+      if (month === monthSelected && year === yearSelected) {
+        try {
+          total += Number(item.amount);
+        } catch {
+          throw new Error('Invalid amount! Amount must be number.');
+        }
+      }
+    });
+
+    return total;
+  }, [monthSelected, yearSelected]);
+
+
+  const totalBalance = useMemo(() => {
+    return totalGains - totalExpenses;
+  }, [totalGains, totalExpenses]);
+
+
+  const message = useMemo(() => {
+    if (totalBalance < 0) {
+      return {
+        title: "Que triste!",
+        description: "Neste mês, você gastou mais do que deveria.",
+        footerText: "Verifique seus gastos e tente cortar algumas coisas desenecessárias.",
+        icon: sadImg
+      }
+    } else if (totalBalance === 0) {
+      return {
+        title: "Ufaa!",
+        description: "Neste mês, você gastou exatamente o que ganhou.",
+        footerText: "Tenha cuidado. No próximo mês tente poupar o seu dinheiro.",
+        icon: grinningImg
+      }
+    } else {
+      return {
+        title: "Muito bem!",
+        description: "Sua carteira está positiva!",
+        footerText: "continue assim. Considere investir o seu saldo.",
+        icon: happyImg
+      }
+    }
+  }, [totalBalance]);
+
+
   const handleMonthSelected = (month: string) => {
     try {
       const parseMonth = Number(month);
       setMonthSelected(parseMonth);
-    } catch (error) {
+    } catch {
       throw new Error('invalid month value. Id accept 0 - 24.');
     }
   };
@@ -90,7 +164,7 @@ const Dashboard: React.FC = () => {
         <WalletBox
           title="Saldo"
           color="#4341F0"
-          amount={150.00}
+          amount={totalBalance}
           footerLabel='Atualizado com base nas entradas e saídas'
           icon="dolar"
         />
@@ -98,7 +172,7 @@ const Dashboard: React.FC = () => {
         <WalletBox
           title="Entradas"
           color="#F7931B"
-          amount={5000.00}
+          amount={totalGains}
           footerLabel='Atualizado com base nas entradas'
           icon="arrowUp"
         />
@@ -106,16 +180,16 @@ const Dashboard: React.FC = () => {
         <WalletBox
           title="Saídas"
           color="#E44C4E"
-          amount={4850.00}
+          amount={totalExpenses}
           footerLabel='Atualizado com base nas saídas'
           icon="arrowDown"
         />
 
         <MessageBox
-          title="Muito bem!"
-          description='Sua carteira está positiva!'
-          footerText='Continue assim. Considere investir o seu saldo!'
-          icon={happyImg}
+          title={message.title}
+          description={message.description}
+          footerText={message.footerText}
+          icon={message.icon}
         />
       </Content>
     </Container>
